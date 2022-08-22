9a636d48a680070abe66d856152d4f1d863c0e38
Yaxpatel123 <87279091+Yaxpatel123@users.noreply.github.com>
2022-08-22 09:40:14 +0530

    Q1

---
+++ src/main/kotlin/PRACTICAL_1_3.kt
@@ -0,0 +1,48 @@
+fun main(){
+
+    print("Enter enrollment number : ")
+    val Enrollment_input = readLine()
+
+
+    print("Enter Name : ")
+    var Name_input = readLine()
+
+
+    print("Enter Branch : ")
+    var Branch_input = readLine()
+
+
+    print("Enter Class : ")
+    var Class_input = readLine()
+
+
+    print("Enter batch : ")
+    var Batch_input = readLine()
+
+
+    print("Enter College name : ")
+    var College_input = readLine()
+
+
+    print("Enter University name : ")
+    var University_input = readLine()
+
+
+    print("Enter age : ")
+    var age_input = readLine()
+
+
+
+    println("***********************************************")
+
+
+    println("Student Enrollment number : $Enrollment_input")
+    println("Student Name : $Name_input")
+    println("Student branch : $Branch_input")
+    println("Student Class : $Class_input")
+    println("Student batch : ")
+    println("Student college name : $College_input")
+    println("Student university name : $University_input")
+    println("Student age : $age_input")
+
+}
