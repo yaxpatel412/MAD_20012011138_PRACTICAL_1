b0db1b4b67399f8540a0996e1ff9bc73fe5a63b8
Yaxpatel123 <87279091+Yaxpatel123@users.noreply.github.com>
2022-08-22 09:30:58 +0530

    Q1

---
+++ src/main/kotlin/PRACTICAL_1_1.kt
@@ -0,0 +1,41 @@
+fun main(){
+
+    print("Enter an integer : ")
+    val myNumber = readLine()!!
+    println("Integer Value : $myNumber")
+
+    print("Enter a Float Value : ")
+    val myFloat = readLine()
+    println("Float Value : $myFloat")
+
+    print("Enter Character : ")
+    val myLetter = readLine()
+    println("Character value : $myLetter")
+
+    print("Enter String value : ")
+    val myText = readLine()
+    println("String value : $myFloat")
+
+    print("Enter Boolean value : ")
+    val myBoolean = readLine()
+    println("Boolean value : $myBoolean")
+
+    print("Enter Double value : ")
+    val myDouble = readLine()
+    println("double value : $myDouble")
+
+    print("Enter Long value : ")
+    val myLong = readLine()
+    println("Long value : $myLong")
+
+    print("Enter Short value : ")
+    val myShort = readLine()
+    println("Short value : $myShort")
+
+    print("Enter Byte value : ")
+    val myByte = readLine()
+    println("Byte value : $myByte")
+
+
+
+}
