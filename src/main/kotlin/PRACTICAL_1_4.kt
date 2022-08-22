d1bc9bf93315a75ac93f77de313108c20d13ef84
Yaxpatel123 <87279091+Yaxpatel123@users.noreply.github.com>
2022-08-22 09:52:18 +0530

    Q1

--- src/main/kotlin/PRACTICAL_1_4.kt
+++ src/main/kotlin/PRACTICAL_1_4.kt
@@ -1,24 +1,16 @@
+import java.util.*
+
 fun main(){
 
-    print("Enter Month number : ")
-    val number = readLine()
-    val Number_input = when (number)
-    {
-        "1" -> "January"
-        "2" -> "February"
-        "3" -> "March"
-        "4" -> "April"
-        "5" -> "May"
-        "6" -> "June"
-        "7" -> "July"
-        "8" -> "August"
-        "9" -> "September"
-        "10" -> "Octomber"
-        "11" -> "November"
-        "12" -> "December"
-        else -> "Invalid Month"
-    }
+    val reader = Scanner(System.`in`)
 
-    println(Number_input)
+    print("Enter Number : ")
+    var num = reader.nextInt()
+
+    if(num % 2 == 0)
+        println("$num is EVEN")
+    else
+        println("$num is ODD")
+
 
 }
