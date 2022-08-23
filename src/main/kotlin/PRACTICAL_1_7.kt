0e7252a229da53764e2ee9c19ef14d7a59073859
Yaxpatel123 <87279091+Yaxpatel123@users.noreply.github.com>
2022-08-23 19:21:57 +0530

    Q1

---
+++ src/main/kotlin/PRACTICAL_1_7.kt
@@ -0,0 +1,14 @@
+fun main() {
+    print("Enter Number: ")
+    var number = readLine()!!.toInt()
+    val factorial = fact(number)
+    println("Factorial of $number = $factorial")
+}
+
+tailrec fun fact(n: Int, temp: Int = 1): Int {
+    return if (n == 1){
+        temp
+    } else {
+        fact(n-1, temp*n)
+    }
+}
