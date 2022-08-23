a42d2d5538b2795aaf6df51c2cd9fbe4544a8265
Yaxpatel123 <87279091+Yaxpatel123@users.noreply.github.com>
2022-08-23 19:11:15 +0530

    Q1

---
+++ src/main/kotlin/PRACTICAL_1_2.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var int = 10
+    val d1: Double = int.toDouble()
+    print(d1)
+
+    var string = "123"
+    val int1 : Int = string.toInt()
+    println(int1)
+
+    var d2: Double = string.toDouble()
+    println(d2)
+
+
+
+}
